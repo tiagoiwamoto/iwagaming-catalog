0ede768ffaba70dac28f34fe936ed0a00bfbb7c5
Tiago Henrique Iwamoto <tiago.iwamoto@gmail.com>
2024-02-06 07:04:51 -0300

    Initial commit

---
+++ src/main/java/br/com/kamehouse/iwagamingcatalog/IwagamingCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.kamehouse.iwagamingcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwagamingCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwagamingCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kamehouse/iwagamingcatalog/IwagamingCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kamehouse.iwagamingcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwagamingCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
